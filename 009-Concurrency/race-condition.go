6498899afb6ba7889e8340977a27b0a3850b266e
João Pedro Furrier <eujppacheco@gmail.com>
2022-12-11 14:07:09 -0300

    func mutex

--- 009-Concurrency/race-condition.go
+++ 009-Concurrency/race-condition.go
@@ -16,12 +16,16 @@ func main() {
 	const gs = 100
 	wg.Add(gs)
 
+	var mu sync.Mutex
+
 	for i := 0; i < gs; i++ {
 		go func() {
+			mu.Lock()
 			v := counter
 			runtime.Gosched()
 			v++
 			counter = v
+			mu.Unlock()
 			wg.Done()
 		}()
 		fmt.Println("Goroutines:", runtime.NumGoroutine())

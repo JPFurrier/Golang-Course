7bff94f58e5a2832ceb0bc4e29675aae548cbcb2
João Pedro Furrier <eujppacheco@gmail.com>
2022-12-14 19:19:24 -0300

    returns a value and an error

---
+++ 002-Exercises/exercise-54.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := toJSON(p1)
+
+	fmt.Println(string(bs))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
+
+// toJSON needs to return an err or also
+func toJSON(a interface{}) ([]byte, error) {
+	bs, err := json.Marshal(a)
+	if err != nil {
+		return []byte{}, fmt.Errorf("There was an error in toJSON: %v", a, err)
+	}
+
+	return bs, nil
+}

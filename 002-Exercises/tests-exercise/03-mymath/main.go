d225416643e8123e81376878b4f1c012b9bb8948
João Pedro Furrier <eujppacheco@gmail.com>
2022-12-19 15:19:06 -0300

    main func for mymath

---
+++ 002-Exercises/tests-exercise/03-mymath/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"go-programming-master/code_samples/013-new-version/002-Exercises/tests-exercise/03-mymath/mymath"
+)
+
+func main() {
+	xxi := gen()
+	for _, v := range xxi {
+		fmt.Println(mymath.CenteredAvg(v))
+	}
+}
+
+// gen generates data to pass into CenteredAvg
+func gen() [][]int {
+	a := []int{1, 4, 6, 8, 100}
+	b := []int{0, 8, 10, 1000}
+	c := []int{9000, 4, 10, 8, 6, 12}
+	d := []int{123, 744, 140, 200}
+	e := [][]int{a, b, c, d}
+	return e
+}
